c255a26bced3b04235f4c6e913222f984e468991
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-11-03 22:11:47 +0000

    improve app initialization

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -1,16 +1,31 @@
 package app
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/franciscosbf/spotify-tui/pkg/ui"
 )
 
+func die(err any) {
+	fmt.Fprintf(os.Stderr, "app error: %s\n", err)
+	os.Exit(1)
+}
+
 func Run() {
-	tui := ui.New("./configs/auth.json")
+	var config string
+
+	flag.StringVar(&config, "config", "<path>", "configuration file")
+	flag.Parse()
+
+	if config == "" {
+		die("config parameter missing")
+	}
+
+	tui := ui.New("./configs/config.json")
 
 	if err := tui.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "app error: %s", err)
+		die(err)
 	}
 }
